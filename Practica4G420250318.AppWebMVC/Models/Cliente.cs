ea4fc47d035dd964b5862d05f6b05d0b44ef4bb4
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-03-18 14:23:15 -0600

    modificando las vista de clientes

--- Practica4G420250318.AppWebMVC/Models/Cliente.cs
+++ Practica4G420250318.AppWebMVC/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Practica4G420250318.AppWebMVC.Models;
 
@@ -7,11 +8,13 @@ public partial class Cliente
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Nombre { get; set; } = null!;
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
 
     public string Apellido { get; set; } = null!;
-
+    [Display(Name = "Dirección")]
     public string? Direccion { get; set; }
-
+    [Display(Name = "Teléfono")]
     public string? Telefono { get; set; }
 }
