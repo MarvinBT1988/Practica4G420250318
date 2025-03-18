dd277efdc122b02f3e60266c612f2b4eccdce229
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-03-18 13:11:22 -0600

    Agregar archivos de proyecto.

---
+++ Practica4G420250318.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica4G420250318.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
